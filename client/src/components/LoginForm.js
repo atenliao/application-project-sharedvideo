e09de00c027171b3d89e3324fda21d5a636c33c1
atenliao <atenliao@gmail.com>
2023-10-07 08:57:38 -0700

    update style

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,64 @@
+import React, {useState} from "react";
+import {Button, Error, Input, FormField, Label} from "../styles";
+
+function LoginForm({onLogin}){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    function handleSubmit(e){
+        e.preventDefault();
+        setIsLoading(true);
+        fetch("/login",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify({username, password}),
+        }).then((res)=>{
+            setIsLoading(false);
+            if(res.ok) {
+                res.json().then((user)=>onLogin(user));
+            }else {
+                res.json().then((err)=>setError(err.error));
+            }
+        });
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <FormField>
+                <Label htmlFor = "username">Username</Label>
+                <Input
+                    type = "text"
+                    id = "username"
+                    autoComplete="off"
+                    value = {username}
+                    onChange={(e)=>setUsername(e.target.value)}
+                />
+            </FormField>
+            <FormField>
+                <Label htmlFor="password">Password</Label>
+                <Input 
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
+                />
+            </FormField>
+            <FormField>
+                <Button variant="fill" color="primary" type = "submit">
+                    {isLoading ? "Loading...":"Login"}
+                </Button>
+            </FormField>
+            <FormField>
+                {errors.map((err)=>(
+                    <Error key={err}>{err}</Error>
+                ))}
+            </FormField>
+        </form>
+    )
+
+}
